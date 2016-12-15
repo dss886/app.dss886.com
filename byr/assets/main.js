2eb3767a39dd27565df51bc1006c7dac770eb207
dss886 <dss886@gmail.com>
2016-12-16 04:10:59 +0800

    modify

--- byr/assets/main.js
+++ byr/assets/main.js
@@ -9,8 +9,8 @@ function is_weixin(){
 
 function ios_click(){
 	if (is_weixin()) {
-		$("#content").css("display", "none");
-		$("#weixin_tip").css("display", "block");
+		$("#weixin-browser").html("选择「在Safari中打开」")
+		$("#bottom-modal").modal("show");
 	} else {
 		window.location.href="https://itunes.apple.com/cn/app/bei-you-ren-lun-tan-bei-you/id1115232927"
 	}
@@ -18,8 +18,8 @@ function ios_click(){
 
 function android_click(){
 	if (is_weixin()) {
-		$("#content").css("display", "none");
-		$("#weixin_tip").css("display", "block");
+		$("#weixin-browser").html("选择「在浏览器中打开」")
+		$("#bottom-modal").modal("show");
 	} else {
 		window.location.href="https://github.com/dss886/BYR-BBS-APP-Release/releases/download/Beta11/BYR-BBS-APP-Beta11-Build18.apk"
 	}
