3dda596c3480451255bb8f2cb8ff0eab0942084d
dss886 <dss886@gmail.com>
2017-04-02 17:55:15 +0800

    更改CNZZ的事件打点

--- byr/assets/main.js
+++ byr/assets/main.js
@@ -13,22 +13,22 @@ function is_weixin(){
 
 function ios_click(){
 	if (is_weixin()) {
-		_czc.push(['_trackEvent', '下载', '下载跳转', 'ios','weixin','ios-btn']);
+		_czc.push(['_trackEvent', 'iOS', '微信提示']);
 		$("#weixin-browser").html("选择「在Safari中打开」")
 		$("#bottom-modal").modal("show");
 	} else {
-		_czc.push(['_trackEvent', '下载', '下载跳转', 'ios','browser','ios-btn']);
+		_czc.push(['_trackEvent', 'iOS', '下载跳转']);
 		window.location.href="https://itunes.apple.com/cn/app/bei-you-ren-lun-tan-bei-you/id1115232927"
 	}
 }
 
 function android_click(){
 	if (is_weixin()) {
-		_czc.push(['_trackEvent', '下载', '下载跳转', 'android','weixin','ios-btn']);
+		_czc.push(['_trackEvent', 'Android', '微信提示']);
 		$("#weixin-browser").html("选择「在浏览器中打开」")
 		$("#bottom-modal").modal("show");
 	} else {
-		_czc.push(['_trackEvent', '下载', '下载跳转', 'android','browser','ios-btn']);
+		_czc.push(['_trackEvent', 'Android', '下载跳转']);
 		window.location.href="https://github.com/dss886/BYR-BBS-APP-Release/releases/download/Beta11/BYR-BBS-APP-Beta11-Build18.apk"
 	}
 }
