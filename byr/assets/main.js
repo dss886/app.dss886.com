21614dc31b33e989fb9e848b9dd332fcead2df24
dss886 <dss886@gmail.com>
2017-04-03 13:02:18 +0800

    add talkingData analysis

--- byr/assets/main.js
+++ byr/assets/main.js
@@ -14,10 +14,12 @@ function is_weixin(){
 function ios_click(){
 	if (is_weixin()) {
 		_czc.push(['_trackEvent', 'iOS', '微信提示']);
+		TDAPP.onEvent("iOS", '微信提示');
 		$("#weixin-browser").html("选择「在Safari中打开」")
 		$("#bottom-modal").modal("show");
 	} else {
 		_czc.push(['_trackEvent', 'iOS', '下载跳转']);
+		TDAPP.onEvent("iOS", '下载跳转');
 		window.location.href="https://itunes.apple.com/cn/app/bei-you-ren-lun-tan-bei-you/id1115232927"
 	}
 }
@@ -25,10 +27,12 @@ function ios_click(){
 function android_click(){
 	if (is_weixin()) {
 		_czc.push(['_trackEvent', 'Android', '微信提示']);
+		TDAPP.onEvent("Android", '微信提示');
 		$("#weixin-browser").html("选择「在浏览器中打开」")
 		$("#bottom-modal").modal("show");
 	} else {
 		_czc.push(['_trackEvent', 'Android', '下载跳转']);
+		TDAPP.onEvent("Android", '下载跳转');
 		window.location.href="https://github.com/dss886/BYR-BBS-APP-Release/releases/download/Beta11/BYR-BBS-APP-Beta11-Build18.apk"
 	}
 }
